83599d445ee77aa1cba615071205d656a1e78d75
Daniel Belarmino <belarba@gmail.com>
2020-05-11 20:33:06 -0600

    all routes working

--- src/app.js
+++ src/app.js
@@ -24,15 +24,56 @@ app.post("/repositories", (request, response) => {
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+    const {id} = request.params;
+    const {title, url, techs} = request.body;
+
+    const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id);
+
+    if (repositorieIndex < 0){
+        return response.status(400).json({ error: 'Repositorie not found'});
+    }
+
+    const likes = repositories[repositorieIndex].likes;
+
+    const repositorie = {
+        id,
+        title,
+        url,
+        techs,
+        likes
+    }
+
+    repositories[repositorieIndex] = repositorie;
+    
+    return response.json(repositorie);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+    const { id } = request.params
+
+    const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id);
+
+    if (repositorieIndex < 0){
+        return response.status(400).json({ error: 'Repositorie not found'});
+    }
+
+    repositories.splice(repositorieIndex,1);
+
+    return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+    const {id} = request.params;
+
+    const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id);
+
+    if (repositorieIndex < 0){
+        return response.status(400).json({ error: 'Repositorie not found'});
+    }
+    
+    repositories[repositorieIndex].likes ++;
+  
+  return response.json(repositories[repositorieIndex]);
 });
 
 module.exports = app;
